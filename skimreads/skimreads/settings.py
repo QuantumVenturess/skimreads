e55301b2bb0bfa6c685fe1bcb679cf276a1643ea
QuantumVenturess <quantumventuress@gmail.com>
2013-01-21 22:51:27 -0800

    fixed database dictionary

--- skimreads/skimreads/settings.py
+++ skimreads/skimreads/settings.py
@@ -50,9 +50,7 @@ else:
     # Parse database configuration from $DATABASE_URL
     import dj_database_url
     DATABASES = {
-        'default': {
-            dj_database_url.config(default=os.environ['DATABASE_URL'])
-        }
+        'default': dj_database_url.config(default=os.environ['DATABASE_URL'])
     }
 
 # Email
