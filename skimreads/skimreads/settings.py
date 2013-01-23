082adbd6c331aefe818c05cccfc8beb2a93e460e
QuantumVenturess <quantumventuress@gmail.com>
2013-01-22 22:08:41 -0800

    debug true, compress_enabled true

--- skimreads/skimreads/settings.py
+++ skimreads/skimreads/settings.py
@@ -4,8 +4,9 @@ import os, socket
 # Check environment
 if os.environ.get('MYSITE_PRODUCTION', False):
     # production
-    DEBUG = TEMPLATE_DEBUG = False
+    DEBUG = TEMPLATE_DEBUG = True
     DEV = False
+    COMRESS_ENABLED = True
 else:
     # development
     DEBUG = TEMPLATE_DEBUG = True
