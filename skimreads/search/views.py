c5fd510f440b345254c63697c8f62daf376609c7
QuantumVenturess <quantumventuress@gmail.com>
2013-03-05 21:50:40 -0800

    removed using=using from indexes.py in haystack folder in line 157; improved search by splitting up the words in query and searching based on those words

--- skimreads/search/views.py
+++ skimreads/search/views.py
@@ -5,14 +5,16 @@ from django.template import loader, RequestContext
 from haystack.query import SearchQuerySet
 
 import json
+import operator
 
 @login_required
 def all(request):
     """Return search results for all readings, notes, topics."""
     query = request.GET.get('q')
     if query:
-        results = SearchQuerySet().filter(
-            Q(content=query) | Q(content__startswith=query)).order_by(
+        queries = [word for word in query.split(' ') if word]
+        qs = [(Q(content=q) | Q(content__startswith=q)) for q in queries]
+        results = SearchQuerySet().filter(reduce(operator.and_, qs)).order_by(
             '-django_ct')[0:10]
     else:
         results = []
